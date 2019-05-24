99cadcd6386931c5775f358f6298e9d2d1de6bd6
LuckyXJ <1539988381@qq.com>
2019-05-24 12:17:46 +0800

    project init

---
+++ vue/Travel/src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- template模板向外暴露只能暴露一个根标签，所以需要用一个标签包裹一下 -->
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>

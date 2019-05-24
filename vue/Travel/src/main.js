b6f5ab5d274489f24166829113d9454108f7cc33
LuckyXJ <1539988381@qq.com>
2019-05-24 15:29:22 +0800

    add header

--- vue/Travel/src/main.js
+++ vue/Travel/src/main.js
@@ -6,9 +6,10 @@ import router from './router'
 /* 解决移动端300毫秒点击延迟问题 */
 import fastClick from 'fastclick'
 /* 重置默认样式，使样式显示在各种浏览器中的效果一致 */
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 /* 解决移动端一像素边框问题 */
-import './assets/styles/border.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)

0363410a8e199e279fb905808b0044f8aa16b6ae
LuckyXJ <1539988381@qq.com>
2019-05-24 14:25:09 +0800

    project init

---
+++ vue/Travel/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+/* 解决移动端300毫秒点击延迟问题 */
+import fastClick from 'fastclick'
+/* 重置默认样式，使样式显示在各种浏览器中的效果一致 */
+import './assets/styles/reset.css'
+/* 解决移动端一像素边框问题 */
+import './assets/styles/border.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
